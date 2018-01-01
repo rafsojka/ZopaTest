04f923b23938d3e4a13dbe14f2d19733310f88ce
rafsojka <raf.sojka@gmail.com>
2018-01-01 20:00:48 +0000

    Nancy.SelfHost working

---
+++ ZopaTest.Nancy.SelfHost/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Nancy;
+using Nancy.Hosting.Self;
+
+
+
+namespace ZopaTest.Nancy.SelfHost
+{
+    // a simple module to be hosted in the console app
+    public class MainModule : NancyModule
+    {
+        public MainModule()
+        {
+            Get["/"] = _ => { return "Hello World from Nancy!"; };
+
+            Get["/{loan_amount}"] = 
+                parameters => 
+                {
+                    var market_file = string.IsNullOrEmpty((string)this.Request.Query["market_file"]) ? @"..\..\..\ZopaTest\market_offers\Market Data for Exercise.csv" : (string)this.Request.Query["market_file"];
+                    var quote = ZopaTest.Interop.getQuote(market_file, parameters.loan_amount);
+
+                    return View["QuoteView", quote];
+                };
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HostConfiguration hostConfigs = new HostConfiguration();
+            hostConfigs.UrlReservations.CreateAutomatically = true;
+
+            // initialize an instance of NancyHost (found in the Nancy.Hosting.Self package)
+            using (var host = new NancyHost(hostConfigs, new Uri("http://localhost:1234")))
+            {
+                host.Start(); // start hosting
+
+                Console.WriteLine("Running on http://localhost:1234");
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ ZopaTest/Interop.fs
@@ -0,0 +1,21 @@
+﻿module ZopaTest.Interop
+
+open ZopaTest.Offers
+open ZopaTest.Quote
+open ZopaTest.Validation
+
+// providing C# friendly interface as exposing FSharp.Data.CsvProvider class is not an option 
+// https://stackoverflow.com/questions/24274760/f-data-csvprovider-use-from-a-c-sharp-application-interop
+
+let getQuote (marketFile:string) loanAmount =
+    validateLoanAmount loanAmount
+
+//    let marketFilePath =
+//        match marketFile with
+//        | null -> "..\..\market_offers\Market Data for Exercise.csv"
+//        | _ -> marketFile
+
+    let currentOffers = Offers.Load(marketFile)
+
+    getQuote currentOffers loanAmount
+
