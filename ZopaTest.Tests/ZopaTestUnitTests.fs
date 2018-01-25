1a591706eefb279b41b5a1d067765ea936a67535
rafsojka <raf.sojka@gmail.com>
2018-01-25 15:56:04 +0000

    implementing lender exposure

--- ZopaTest.Tests/ZopaTestUnitTests.fs
+++ ZopaTest.Tests/ZopaTestUnitTests.fs
@@ -10,6 +10,43 @@ open ZopaTest.Calculations
 [<TestFixture>]
 type ZopaTestUnitTests() = 
 
+    static member getOffersSumTestCaseSource = [
+      TestCaseData(None, 2330)
+      TestCaseData(Some 200, 1170)
+    ]
+
+    //[<TestCase((None : int option), 200)>]  //TestCase unable to accept None, using TestCaseSource instead
+    [<TestCaseSource("getOffersSumTestCaseSource")>]
+    member test.``getOffersSum calculates sum based on exposure`` (exposure : int option, expectedOffersSum) =
+        let sampleCsv = """
+            Lender,Rate,Available
+            Bob,0.075,640
+            Jane,0.069,480
+            Fred,0.071,520
+            Mary,0.104,170
+            John,0.081,320
+            Dave,0.074,140
+            Angela,0.071,60 """
+
+        let offers = Offers.Parse(sampleCsv)
+        let offersSum = getOffersSum offers exposure
+
+        offersSum               |> should equal expectedOffersSum
+
+    static member getAmountFromCurrentLender = 
+        [|
+            [| {Requested = 1000; Exposure = None; Rate = 0.0M; Available = 0}, ("Bob", 0.075M, 640), 640|]
+            [| {Requested = 1000; Exposure = Some 200; Rate = 0.0M; Available = 0}, ("Bob", 0.075M, 640), 200|]
+            [| {Requested = 1000; Exposure = Some 200; Rate = 5.0M; Available = 900}, ("Bob", 0.075M, 640), 100|]
+        |]
+
+    [<TestCaseSource("getAmountFromCurrentLender")>]
+    member test.``getAmountFromCurrentLender calculates amount from lender based on exposure`` (testData : OfferAccumulator * Offers.Row * int) =
+        let accumulator, row, expectedAmountFromCurrentLender = testData
+        let amountFromCurrentLender = getAmountFromCurrentLender accumulator row
+
+        amountFromCurrentLender     |> should equal expectedAmountFromCurrentLender
+
     [<TestCase( 2400)>]
     [<TestCase( 12400)>]
     member test.``calculateTheBestRateBasedOnOffers returns None`` (loanAmount) =
@@ -28,6 +65,29 @@ type ZopaTestUnitTests() =
 
         bestRate.IsNone             |> should equal true
 
+    static member calculateWithExposureReturnsNoneTestData = [
+      TestCaseData(1200, Some 200)
+      TestCaseData(2000, Some 200)
+      TestCaseData(2400, Some 200)
+    ]
+
+    [<TestCaseSource("calculateWithExposureReturnsNoneTestData")>]
+    member test.``calculateTheBestRateBasedOnOffersWithExposure returns None`` (loanAmount, exposure) =
+        let sampleCsv = """
+            Lender,Rate,Available
+            Bob,0.075,640
+            Jane,0.069,480
+            Fred,0.071,520
+            Mary,0.104,170
+            John,0.081,320
+            Dave,0.074,140
+            Angela,0.071,60 """
+
+        let offers = Offers.Parse(sampleCsv)
+        let bestRate = calculateTheBestRateBasedOnOffersWithExposure offers loanAmount exposure
+
+        bestRate.IsNone             |> should equal true
+
     // test values calculated using compounded_loan_rate.xlsx
     [<TestCase( "loanAmount within best lender availability",1000, 0.069)>]
     [<TestCase( "loanAmount spread between two best lenders 50/50",2000, 0.07)>]
@@ -35,7 +95,7 @@ type ZopaTestUnitTests() =
     [<TestCase( "loanAmount spread between two best lenders 33.(3)/66.(6)",3000, 0.070263)>]
     [<TestCase( "loanAmount spread between three best lenders 25/50/25",4000, 0.07125)>]
     [<TestCase( "loanAmount spread between all six lenders 10/20/10/20/20/20",10000, 0.0805)>]
-    member test.``calculateTheBestRateBasedOnOffers finds the best rate`` (testCaseDesc, loanAmount, expectedRate) =
+    member test.``calculateTheBestRateBasedOnOffersWithExposure finds the best rate`` (testCaseDesc, loanAmount, expectedRate) =
         let sampleCsv = """
             Lender,Rate,Available
             Bob,0.075,2000
@@ -49,7 +109,31 @@ type ZopaTestUnitTests() =
         let bestRate = calculateTheBestRateBasedOnOffers offers loanAmount
 
         bestRate.IsSome             |> should equal true
-        bestRate.Value              |> should (equalWithin 0.005) expectedRate
+        bestRate.Value              |> should (equalWithin 0.0005) expectedRate
+
+    static member calculateWithExposureFindsBestRateTestData = [
+      TestCaseData("loanAmount equals 5 x exposure", 1000, Some 200, 0.074)
+      TestCaseData("loanAmount equals 2 x exposure", 1000, Some 500, 0.07)
+      TestCaseData("loanAmount equals 2.5 x exposure", 1500, Some 600, 0.0708)
+      TestCaseData("loanAmount equals 4 x exposure", 2000, Some 500, 0.07225)
+    ]
+
+    [<TestCaseSource("calculateWithExposureFindsBestRateTestData")>]
+    member test.``calculateTheBestRateBasedOnOffers finds the best rate`` (testCaseDesc, loanAmount, exposure, expectedRate) =
+        let sampleCsv = """
+            Lender,Rate,Available
+            Bob,0.075,2000
+            Jane,0.069,1000
+            Fred,0.071,2000
+            Mary,0.104,2000
+            John,0.081,2000
+            Dave,0.074,1000 """
+
+        let offers = Offers.Parse(sampleCsv)
+        let bestRate = calculateTheBestRateBasedOnOffersWithExposure offers loanAmount exposure
+
+        bestRate.IsSome             |> should equal true
+        bestRate.Value              |> should (equalWithin 0.0005) expectedRate
         
     // test values calculated using compounded_loan_rate.xlsx
     [<TestCase(1000, 0.07004, 12, 3, 30.88, 1111.64)>]
@@ -63,3 +147,8 @@ type ZopaTestUnitTests() =
 
         fst repayments      |> should (equalWithin 0.005) expectedMonthlyRepayment
         snd repayments      |> should (equalWithin 0.005) expectedTotalRepayment
+
+    
+
+   
+
--- ZopaTest/Calculations.fs
+++ ZopaTest/Calculations.fs
@@ -5,33 +5,50 @@ open ZopaTest.Offers
 // accumulator type to be passed through scan method when calculating the accumulated offer
 type OfferAccumulator =
     { Requested     : int
+      Exposure      : int option
       Rate          : decimal
       Available     : int  }
     static member Default =
         {   Requested = 0
+            Exposure = None
             Rate = 0.0M
             Available = 0}
 
+let getAmountFromCurrentLender (offerAccumulator:OfferAccumulator) (row:Offers.Row) =
+    // get what is available from the current lender based on exposure
+    let currentLenderAvailable =
+        match offerAccumulator.Exposure with
+        | Some ex -> min ex row.Available
+        | None -> row.Available
+    // if we are still missing on accumulated total more than current lender can offer, take all his offer
+    if offerAccumulator.Available + currentLenderAvailable <= offerAccumulator.Requested then
+        currentLenderAvailable
+    // otherwise take just what we are missing to total
+    else
+        offerAccumulator.Requested - offerAccumulator.Available
+
 let findAccumulativeRateFromCurrentLender (offerAccumulator:OfferAccumulator) (row:Offers.Row) =
-    let amountFromCurrentLender = 
-        // if we are still missing on accumulated total more than current lender can offer, take all his offer
-        if offerAccumulator.Available + row.Available <= offerAccumulator.Requested then
-            row.Available
-        // otherwise take just what we are missing to total
-        else 
-            offerAccumulator.Requested - offerAccumulator.Available
+    // get max possible amount from the current lender
+    let amountFromCurrentLender = getAmountFromCurrentLender offerAccumulator row
     // calculate partial percentage of total amount requested from the current lender
     let percentOfTotalFromCurrentLender = (decimal)amountFromCurrentLender / (decimal)offerAccumulator.Requested * 100.0M
     // calculate partial percentage of rate from current lender
     let rateFromCurrentLender = row.Rate * (decimal)percentOfTotalFromCurrentLender / 100.0M
     // update and return the totals
     {   Requested = offerAccumulator.Requested 
+        Exposure = offerAccumulator.Exposure
         Rate = offerAccumulator.Rate + rateFromCurrentLender
         Available = offerAccumulator.Available + amountFromCurrentLender }
 
-let calculateTheBestRateBasedOnOffers (offers:Offers) requestedAmount =
+let getOffersSum (offers:Offers) exposure =
+    // calculate the sum of offers from lenders based on exposure
+    match exposure with
+    | Some ex -> offers.Rows |> Seq.map(fun o -> min o.Available ex) |> Seq.sum
+    | None _ -> offers.Rows |> Seq.sumBy(fun o -> o.Available)
+
+let calculateTheBestRateBasedOnOffersWithExposure (offers:Offers) requestedAmount exposure =
     // calculate the sum of offers
-    let offersSum = offers.Rows |> Seq.sumBy(fun o -> o.Available)
+    let offersSum = getOffersSum offers exposure
 
     // check if the sum is enough to match the amount requested
     match offersSum >= requestedAmount with
@@ -41,12 +58,20 @@ let calculateTheBestRateBasedOnOffers (offers:Offers) requestedAmount =
                     // sort by best lenders in terms of rate
                     |> Seq.sortBy(fun o -> o.Rate)
                     // find the rate accumulatively per lender
-                    |> Seq.scan(findAccumulativeRateFromCurrentLender) { OfferAccumulator.Default with Requested = requestedAmount }
+                    |> Seq.scan(findAccumulativeRateFromCurrentLender) { OfferAccumulator.Default with 
+                                                                                    Requested = requestedAmount 
+                                                                                    Exposure = exposure }
                     // iterate sequence only if there is still work to do (we have not accumulated the sum matching the requested amount)
                     |> Seq.takeWhile(fun oa -> oa.Available <= requestedAmount)
                     // take the last element from the sequence - the final accumulated offer
                     |> Seq.last
                 Some <| finalAccumulatedRate.Rate
+
+// specialised version with exposure ignored "baked in" for backwards compatibility
+let calculateTheBestRateBasedOnOffers offers requestedAmount = calculateTheBestRateBasedOnOffersWithExposure offers requestedAmount None
+
+// specialised version with standard (£200) exposure "baked in"
+let calculateTheBestRateBasedOnOffersWithStandardExposure offers requestedAmount = calculateTheBestRateBasedOnOffersWithExposure offers requestedAmount (Some 200)
 
 // formula taken from https://www.thebalance.com/loan-payment-calculations-315564
 let calculateRepayments (A:int) (R:decimal) (ppy:int) (y:int) =
